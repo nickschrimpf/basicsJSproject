427b6327067e82ca25c08736f46a5571d5b09895
Nicholas Schrimpf <nickschrimpf@Nicholass-MacBook-Pro.local>
2022-02-18 09:19:08 -0700

    refactoring adding conditional statements

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,39 +1,58 @@
 let defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = []
 
 
 addBtn.addEventListener('click',add);
 subtractBtn.addEventListener('click',subtract)
 multiplyBtn.addEventListener('click',multiply)
 divideBtn.addEventListener('click',devide)
+
+//grabs input from input field
 function grabUserNumberInput(){
     return parseInt(userInput.value)
 }
-function createAndWriteOutput(operator,resultBeforeCalc, calcNumber){
-    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`
-    outputResult(currentResult,calcDescription)
+//creates and writes to the dom
+function createAndWriteOutput(operator,initalResult, enteredNumber){
+    const calcDescription = `${initalResult} ${operator} ${enteredNumber}`
+    outputResult(currentResult,calcDescription)//from vendor.js
+    writeToLog(operator,initalResult,enteredNumber,currentResult)
+    console.log(logEntries)
 }
-function add(num1, num2){
+function writeToLog(operation,resultBeforeCalc,enteredNumber,currentResult){
+    const logEntry = {
+        operation: operation,
+        previousResult:resultBeforeCalc,
+        number:enteredNumber,
+        result:currentResult
+    }
+    logEntries.push(logEntry)
+}
+function calculateResult(operation){
     const enteredNumber = grabUserNumberInput();
     const initialResult = currentResult
-    currentResult = currentResult + enteredNumber;
-    createAndWriteOutput('+',initialResult,enteredNumber)
+    if(operation === '+'){
+        currentResult += enteredNumber;//currentResult = currentResult + enteredNumber;
+    }else if (operation === '-'){
+        currentResult -= enteredNumber;
+    }else if (operation === '*'){
+        currentResult *= enteredNumber;
+    }else if (operation === '/'){
+        currentResult /= enteredNumber;
+    }
+    
+    createAndWriteOutput(operation,initialResult,enteredNumber) 
+}
+function add(num1, num2){
+    calculateResult('+')
 }
 function subtract(num1,num2){
-    const enteredNumber = grabUserNumberInput();
-    const initialResult = currentResult;
-    currentResult = currentResult - enteredNumber;
-    createAndWriteOutput('-',initialResult,enteredNumber)
+    calculateResult('-')         
 }
 function multiply(num1, num2){
-    const enteredNumber = grabUserNumberInput()
-    const initialResult = currentResult;
-    currentResult = currentResult * enteredNumber;
-    createAndWriteOutput('*',initialResult,enteredNumber)
+    calculateResult('*')
+    
 }
 function devide(num1,num2){
-    const enteredNumber = grabUserNumberInput();
-    const initialResult = currentResult;
-    currentResult = currentResult / enteredNumber;
-    createAndWriteOutput('/',initialResult,enteredNumber)
+    calculateResult('/')
 }
